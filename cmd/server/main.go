9efb2d7b2e6a88b3fcd848916b2ac5852b835f8b
Esther Kim <jabbukka@naver.com>
2021-12-11 10:55:15 +0900

    implement: setLogger func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"path"
+	"runtime"
 
 	"github.com/estherk0/slack-ae-bot/pkg/routes"
 	"github.com/gin-gonic/gin"
@@ -10,6 +13,7 @@ import (
 )
 
 func main() {
+	setLogger()
 	engine := gin.Default()
 	engine.Use(gin.Recovery())
 	routes.Register(engine)
@@ -28,4 +32,24 @@ func main() {
 		logrus.Fatalf("ListenAndServe has been failed. Error %s", err.Error())
 		panic(err)
 	}
+}
+
+func setLogger() {
+	// logrus
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
+			filename := path.Base(f.File)
+			return "", fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+	})
+	if os.Getenv("ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+		logrus.SetLevel(logrus.InfoLevel)
+	} else {
+		gin.SetMode(gin.DebugMode)
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logrus.SetOutput(os.Stdout)
 }
